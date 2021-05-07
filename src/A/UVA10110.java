21d00f082b34e1b720c4cd40b1bf312f7e972198
Huthayfa0 <huthifa0@gmail.com>
2021-05-07 14:23:26 +0300

    UVA 10110

--- src/A/UVA10110.java
+++ src/A/UVA10110.java
@@ -7,30 +7,13 @@ public class UVA10110 {
         Scanner scan=new Scanner(System.in);
         int n=scan.nextInt();
         while (n!=0){
-            System.out.println(solve(n)?"Yes":"No");
+            System.out.println(solve(n)?"yes":"no");
             n=scan.nextInt();
         }
     }
     public static boolean solve(int n) {
-        int count = 0;
+        int x=(int)Math.sqrt(n);
 
-        int x;
-        for(x = n; (x & 1) == 0; ++count) {
-            x /= 2;
-        }
-
-        int ans = count;
-
-        for(int i = 3; i <= x; i += 2) {
-            for(count = 0; x % i == 0; ++count) {
-                x /= i;
-            }
-
-            if (count != 0) {
-                ans = (ans + 1) * (count + 1);
-            }
-        }
-
-        return (ans & 1) == 1;
+        return x*x==n;
     }
 }
