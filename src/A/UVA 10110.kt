f023a6359079d472bbc53dd9cb26e7e7bd84acf9
Huthayfa0 <huthifa0@gmail.com>
2021-05-11 15:10:32 +0300

    UVA 10110

--- src/A/UVA 10110.kt
+++ src/A/UVA 10110.kt
@@ -14,24 +14,6 @@ fun main() {
 }
 
 fun solve(n: Int): Boolean {
-    var count=0
-    var x=n
-
-    while (x and 1==0){
-        x/=2
-        count++
-    }
-    var ans=count
-    var i=3
-    while (i<=x){
-        count=0
-        while (x%i==0){
-            x/=i
-            count++
-        }
-        if (count!=0)
-            ans=(ans+1)*(count+1)
-        i+=2
-    }
-    return ans and 1==1
+    val x = Math.sqrt(n.toDouble()).toInt()
+    return x * x == n
 }
