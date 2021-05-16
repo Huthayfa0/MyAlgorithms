d0cc7a803ec8669d4a94ff6dffca9789c209b13c
Huthayfa0 <huthifa0@gmail.com>
2021-05-17 00:24:51 +0300

    SPOJ MAKETREE

---
+++ src/A/SPOJ MAKETREE.kt
@@ -0,0 +1,37 @@
+package A
+
+val ans= mutableListOf<Int>()
+fun main() {
+    val (n,k)= readLine()!!.split(" ").map { it.toInt() }
+    val adjacency=Array(n){ mutableSetOf<Int>()}
+    repeat(k){
+        val arr= readLine()!!.split(" ").map {x->x.toInt() }
+        for (i in 1 until arr.size){
+            adjacency[it].add(arr[i]-1)
+        }
+    }
+    val vis=BooleanArray(n)
+
+    for (i in vis.indices){
+        if (!vis[i]){
+            dfs(i,vis,adjacency)
+        }
+    }
+    val arr=IntArray(n)
+    var p=0
+    for (x in ans.size-1 downTo 0){
+        arr[ans[x]]=p
+        p=ans[x]+1
+    }
+    println(arr.joinToString (separator = "\n"))
+}
+
+fun dfs(i: Int, vis: BooleanArray, adjacency: Array<MutableSet<Int>>) {
+    vis[i]=true
+    for (x in adjacency[i]){
+        if (!vis[x]){
+            dfs(x,vis,adjacency)
+        }
+    }
+    ans.add(i)
+}
