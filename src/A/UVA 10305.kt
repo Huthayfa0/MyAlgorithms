1a9d232f55947739092c238177183db3c58f0e4c
Huthayfa0 <huthifa0@gmail.com>
2021-05-17 14:11:23 +0300

    UVA 10305

---
+++ src/A/UVA 10305.kt
@@ -0,0 +1,29 @@
+package A
+
+val an= mutableListOf<Int>()
+fun main() {
+    val (n,k)= readLine()!!.split(" ").map { it.toInt() }
+    val adjacency=Array(n){ mutableSetOf<Int>()}
+    repeat(k){
+        val (i,j)= readLine()!!.split(" ").map {x->x.toInt() }
+        adjacency[i-1].add(j-1)
+    }
+    val vis=BooleanArray(n)
+
+    for (i in vis.indices){
+        if (!vis[i]){
+            dfss(i,vis,adjacency)
+        }
+    }
+    println(an.reversed().joinToString(separator = " ") {x->""+(x+1)})
+}
+
+fun dfss(i: Int, vis: BooleanArray, adjacency: Array<MutableSet<Int>>) {
+    vis[i]=true
+    for (x in adjacency[i]){
+        if (!vis[x]){
+            dfss(x,vis,adjacency)
+        }
+    }
+    an.add(i)
+}
