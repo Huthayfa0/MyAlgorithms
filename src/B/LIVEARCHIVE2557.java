5bd88c7b5c17b6c6fc417b683c7d662c9b114b49
Huthayfa0 <huthifa0@gmail.com>
2021-07-02 20:58:24 +0300

    LIVEARCHIVE 2557

---
+++ src/B/LIVEARCHIVE2557.java
@@ -0,0 +1,15 @@
+package B;
+
+import java.util.Scanner;
+
+public class LIVEARCHIVE2557 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        while (t-->0){
+            int n=s.nextInt();
+            System.out.println((int)Math.sqrt(n));
+
+        }
+    }
+}
