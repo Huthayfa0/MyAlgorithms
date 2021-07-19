3768477f6cb05c4ade89a2e387519bad6484c0b5
Huthayfa0 <huthifa0@gmail.com>
2021-07-19 22:46:54 +0300

    combinations

--- src/numbers/Combinations.kt
+++ src/numbers/Combinations.kt
@@ -29,6 +29,19 @@ fun combinationsIteration(n:Int, m:Int, f:(LinkedList<out Int>)->Unit){
 
     getCombination()
 }
+
+fun getCombinationCountArray(n:Int,m:Int){
+    require(n>=m)
+    val arr=Array(m+1){LongArray(n+1)}
+    for (i in 0 until m+1)
+        for (j in 0 until n+1)
+            arr[i][j]=when{
+                j==0->1
+                i==0->0
+                else-> arr[i-1][j-1]+arr[i-1][j]
+            }
+
+}
 fun main(){
     combinationsIteration(4,2){ println(it.joinToString())}
 }
