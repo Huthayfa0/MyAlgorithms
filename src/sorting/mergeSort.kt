c05918a6fb047c62e7d1efcec0e6b057d2c60a19
Huthayfa0 <huthifa0@gmail.com>
2021-07-19 17:40:50 +0300

    mergeSort

---
+++ src/sorting/mergeSort.kt
@@ -0,0 +1,38 @@
+package sorting
+fun mergeSort(list: List<Int>): List<Int> {
+    if (list.size <= 1) {
+        return list
+    }
+    val middle = list.size / 2
+    val left = list.subList(0,middle)
+    val right = list.subList(middle,list.size)
+
+    return merge(mergeSort(left), mergeSort(right))
+}
+
+fun merge(left: List<Int>, right: List<Int>): List<Int>  {
+    var indexLeft = 0
+    var indexRight = 0
+    val newList : MutableList<Int> = mutableListOf()
+
+    while (indexLeft < left.count() && indexRight < right.count()) {
+        if (left[indexLeft] <= right[indexRight]) {
+            newList.add(left[indexLeft])
+            indexLeft++
+        } else {
+            newList.add(right[indexRight])
+            indexRight++
+        }
+    }
+
+    while (indexLeft < left.size) {
+        newList.add(left[indexLeft])
+        indexLeft++
+    }
+
+    while (indexRight < right.size) {
+        newList.add(right[indexRight])
+        indexRight++
+    }
+    return newList
+}
