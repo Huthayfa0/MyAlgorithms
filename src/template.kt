f9ad0fdd7bbe56140f1a5d7fc17d4e0382da7074
Huthayfa0 <huthifa0@gmail.com>
2021-07-22 00:33:27 +0300

    Template update added lines

--- src/template.kt
+++ src/template.kt
@@ -484,3 +484,34 @@ fun getAng(a:Complex, b:Complex, c:Complex):Double {
     return ang
 }
  */
+
+//lines
+
+/*
+
+fun isCollinear(a: Complex, b: Complex, c: Complex): Boolean {
+    return (crossProduct(b - a, c - a)) ==0.0
+}
+
+fun isPointOnRay(p0: Complex, p1: Complex, p2: Complex): Boolean {
+    return if ((p2 - p0).length() ==0.0) true else ((p1 - p0).normalize()== (p2 - p0).normalize())
+}
+
+fun isPointOnSegment(a: Complex, b: Complex, c: Complex): Boolean {
+    return isPointOnRay(a, b, c) && isPointOnRay(b, a, c)
+}
+fun distToLine(p0: Complex, p1: Complex, p2: Complex): Double {
+    return abs(crossProduct(p1 - p0, p2 - p0) / (p0 - p1).length()) // area = 0.5*b*h
+}
+
+fun distToSegment(p0: Complex, p1: Complex, p2: Complex): Double {
+    var d1: Double
+    var d2: Double
+    val v1: Complex = p1 - p0
+    val v2: Complex = p2 - p0
+    if (dotProduct(v1, v2).also { d1 = it } <= 0) return (p2 - p0).length()
+    if (dotProduct(v1, v1).also { d2 = it } <= d1) return (p2 - p1).length()
+    val t = d1 / d2
+    return (p2 - (p0 + v1 * t)).length()
+}
+ */
