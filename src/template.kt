7a59b724e672661257a32c4131b1d2580f14eb88
Huthayfa0 <huthifa0@gmail.com>
2021-07-21 22:08:34 +0300

    template update. added Complex

--- src/template.kt
+++ src/template.kt
@@ -105,7 +105,7 @@ private fun dfs(size: Int, adjacencyList: List<List<List<Int>>>, v: Int = 0) {
 }
 
 private fun bfs(size: Int, adjacencyList: List<List<List<Int>>>, v: Int = 0) {
-    val searchOrder: MutableList<Int> = java.util.ArrayList()
+    val searchOrder: MutableList<Int> = ArrayList()
     val parent = IntArray(size) { -1 } // Initialize parent[i] to -1
     val queue = LinkedList<Int>() // list used as a queue
     val isVisited = BooleanArray(size)
@@ -345,3 +345,108 @@ private fun BSfindLastFalse(start: Int, end: Int, f:(Int)->Boolean): Int {
     return s
 }
 
+//Complex
+/*
+private fun isEven(i: Int) = i % 2 == 0
+private val img = Complex(0.0, 1.0)
+private fun abs(c: Complex): Double = c.abs()
+private fun exp(c: Complex): Complex {
+    val e = exp(c.real)
+    return Complex(e * cos(c.img), e * sin(c.img))
+}
+private fun sinh(c: Complex) = (exp(c) - exp(-c)) / 2
+private fun cosh(c: Complex) = (exp(c) + exp(-c)) / 2
+private fun tanh(c: Complex) = sinh(c) / cosh(c)
+private fun coth(c: Complex) = cosh(c) / sinh(c)
+private fun cos(c: Complex) = (exp(img * c) + exp(-img * c)) / 2.0
+private fun sin(c: Complex) = img * (exp(-img * c) - exp(img * c)) / 2.0
+private fun tan(c: Complex) = sin(c) / cos(c)
+private fun cot(c: Complex) = cos(c) / sin(c)
+private fun sec(c: Complex) = Complex.ONE / cos(c)
+private fun ln(c: Complex) = Complex(ln(c.abs()), c.phase())
+private fun roots(n: Int) =
+    (1 ..n).map { exp(img*2*PI*it/n) }
+private operator fun Number.plus(c: Complex) = Complex(this.toDouble() + c.real, c.img)
+private operator fun Number.minus(c: Complex) = Complex(this.toDouble() - c.real, -c.img)
+private operator fun Number.times(c: Complex) = Complex(this.toDouble() * c.real, this.toDouble() * c.img)
+private operator fun Number.div(c: Complex) = Complex.ONE / c
+private class Complex(val real: Double, val img: Double) {
+    constructor(real: Number, img: Number) : this(real.toDouble(), img.toDouble())
+    override fun equals(other: Any?): Boolean {
+        return (other is Complex && real == other.real && img == other.img)
+    }
+    override fun hashCode(): Int {
+        return real.hashCode() * 31 + img.hashCode()
+    }
+    operator fun unaryMinus() = Complex(-real, -img)
+    operator fun plus(c: Complex) = Complex(real + c.real, img + c.img)
+    operator fun plus(n: Number) = Complex(real + n.toDouble(), img)
+    operator fun minus(c: Complex) = Complex(real - c.real, img - c.img)
+    operator fun minus(n: Number) = Complex(real - n.toDouble(), img)
+    operator fun times(c: Complex) = Complex(real * c.real - img * c.img, real * c.img + img * c.real)
+    operator fun times(n: Number) = Complex(n.toDouble() * real, n.toDouble() * img)
+    operator fun div(n: Number) = Complex(real / n.toDouble(), img / n.toDouble())
+    operator fun div(c: Complex): Complex {
+        val den = c.normSquared()
+        if (isPracticallyZero(den)) {
+            return this / 0 //TO make this consistent with division by zero number
+        }
+        val num = this * c.conjugate()
+        return num / den
+    }
+    operator fun component1() = real
+    operator fun component2() = img
+    /**
+     * Complex conjugate = x-y*i
+     */
+    fun conjugate() = Complex(real, -img)
+    fun normSquared() = real * real + img * img
+    fun abs(): Double = sqrt(this.normSquared())
+    fun phase(): Double = atan(img / real)
+    fun pow(a: Double) = exp(ln(this) * a)
+    fun pow(a: Number) = exp(ln(this) * a)
+    fun pow(a: Complex) = exp(ln(this) * a)
+    override fun toString(): String {
+        return when {
+            isPracticallyZero(img) -> "$real"
+            isPracticallyZero(real) -> "${img}i"
+            img < 0 -> "$real-${-img}i"
+            else -> "${real}+${img}i"
+        }
+    }
+    private fun isPracticallyZero(d: Double) = abs(d) < DEFAULT_TOLERANCE
+    companion object {
+        /**
+         * Complex 0 = 0 + 0i
+         */
+        val ZERO = Complex(0.0, 0.0)
+        /**
+         * Complex 1 = 1 + 0i
+         */
+        val ONE = Complex(1.0, 0.0)
+        const val DEFAULT_TOLERANCE = 1.0E-15
+        fun fromNumber(n: Number) = Complex(n.toDouble(), 0.0)
+        fun fromPolar(radius: Double, theta: Double) :Complex  =radius*exp(img*theta)
+    }
+    /**
+     * Tests if the norm of the complex number is smaller than the given tolerance
+     */
+    fun isZero(tolerance: Double) = this.abs() < tolerance
+    infix fun to(exponent: Int): Complex {
+        if (exponent == 0) {
+            return ONE
+        }
+        if (exponent == 1) {
+            return this
+        }
+        val half = to(exponent / 2)
+        return if (isEven(exponent)) {
+            half * half
+        } else {
+            half * half * this
+        }
+    }
+    infix fun to(exponent: Complex) = this.pow(exponent)
+    infix fun to(exponent: Number) = this.pow(exponent)
+}
+*/
