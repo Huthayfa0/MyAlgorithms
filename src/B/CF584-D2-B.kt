43ec59b35d3bb69bf897230cbb3af2d632b1a43d
Huthayfa0 <huthifa0@gmail.com>
2021-07-23 12:24:20 +0300

    CF584-D2-B

---
+++ src/B/CF584-D2-B.kt
@@ -0,0 +1,532 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val q=1E9.toLong()+7
+    val n=readlnInt()
+    fun binpow(a:Long, b:Int):Long {
+        if (b == 0)
+            return 1
+        val res = binpow(a, b / 2);
+        if ((b and 1)==1)
+            return (((res * res)%q) * a)%q
+        else
+            return (res * res)%q
+    }
+    val ans=((binpow(27,n)%q)-(binpow(7,n)%q))%q
+    println(if (ans<0) ans+q else ans)
+}
+
+//Scanner code
+private fun readln() = readLine()!!
+private fun readlnInt() = readln().toInt()
+private fun readlnLong() = readln().toLong()
+private fun readlnDouble() = readln().toDouble()
+private fun readlnBigInt(radix: Int = 10) = readln().toBigInteger(radix)
+private fun readlnBigDecimal() = readln().toBigDecimal()
+private fun lineSequence(limit: Int = Int.MAX_VALUE) = generateSequence { readLine() }.constrainOnce().take(limit)
+private fun readlnStrings() = readln().split(' ')
+private fun readlnInts() = readlnStrings().map { it.toInt() }
+private fun readlnLongs() = readlnStrings().map { it.toLong() }
+private fun readlnDoubles() = readlnStrings().map { it.toDouble() }
+private fun readIntArray() = readlnStrings().run { IntArray(size) { get(it).toInt() } }
+private fun readLongArray() = readlnStrings().run { LongArray(size) { get(it).toLong() } }
+private fun readDoubleArray() = readlnStrings().run { DoubleArray(size) { get(it).toDouble() } }
+private fun readlnIntArray(n: Int) = IntArray(n) { readlnInt() }
+private fun readlnLongArray(n: Int) = LongArray(n) { readlnLong() }
+private fun readlnDoubleArray(n: Int) = DoubleArray(n) { readlnDouble() }
+private fun readLongArray2d(rows: Int, cols: Int) = Array(rows) { readLongArray().also { require(it.size == cols) } }
+private fun readIntArray2d(rows: Int, cols: Int) = Array(rows) { readIntArray().also { require(it.size == cols) } }
+private fun readDoubleArray2d(rows: Int, cols: Int) =
+    Array(rows) { readDoubleArray().also { require(it.size == cols) } }
+
+private fun isWhiteSpace(c: Char) = c in " \r\n\t"
+
+// JVM-only targeting code follows next
+// readString() via sequence is still slightly faster than Scanner
+private fun readString() = generateSequence { System.`in`.read().toChar() }
+    .dropWhile { isWhiteSpace(it) }.takeWhile { !isWhiteSpace(it) }.joinToString("")
+
+private fun readInt() = readString().toInt()
+private fun readLong() = readString().toLong()
+private fun readDouble() = readString().toDouble()
+private fun readBigInt(radix: Int = 10) = readString().toBigInteger(radix)
+private fun readBigDecimal() = readString().toBigDecimal()
+private fun readInts(n: Int) = generateSequence { readInt() }.take(n)
+private fun readLongs(n: Int) = generateSequence { readLong() }.take(n)
+private fun readDoubles(n: Int) = generateSequence { readDouble() }.take(n)
+
+
+//sort
+private fun mergeSort(list: List<Int>): List<Int> {
+    fun merge(left: List<Int>, right: List<Int>): List<Int> {
+        var indexLeft = 0
+        var indexRight = 0
+        val newList: MutableList<Int> = mutableListOf()
+
+        while (indexLeft < left.count() && indexRight < right.count()) {
+            if (left[indexLeft] <= right[indexRight]) {
+                newList.add(left[indexLeft])
+                indexLeft++
+            } else {
+                newList.add(right[indexRight])
+                indexRight++
+            }
+        }
+
+        while (indexLeft < left.size) {
+            newList.add(left[indexLeft])
+            indexLeft++
+        }
+
+        while (indexRight < right.size) {
+            newList.add(right[indexRight])
+            indexRight++
+        }
+        return newList
+    }
+    if (list.size <= 1) {
+        return list
+    }
+
+    val middle = list.size / 2
+    val left = list.subList(0, middle)
+    val right = list.subList(middle, list.size)
+
+    return merge(mergeSort(left), mergeSort(right))
+}
+
+
+//Graphs
+private fun dfs(size: Int, adjacencyList: List<List<Int>>, v: Int = 0) {
+    val searchOrder = ArrayList<Int>()
+    val parent = IntArray(size) { -1 }
+
+    // Mark visited vertices
+    val isVisited = BooleanArray(size)
+    fun dfs(v: Int) {
+        // Store the visited vertex
+        searchOrder.add(v)
+        isVisited[v] = true
+        adjacencyList[v].forEach { e ->
+            if (!isVisited[e]) {
+                parent[e] = v
+                dfs(e)
+            }
+        }
+    }
+    // Recursively search
+    dfs(v)
+}
+
+private fun bfs(size: Int, adjacencyList: List<List<Int>>, v: Int = 0) {
+    val searchOrder: MutableList<Int> = ArrayList()
+    val parent = IntArray(size) { -1 } // Initialize parent[i] to -1
+    val queue = LinkedList<Int>() // list used as a queue
+    val isVisited = BooleanArray(size)
+    queue.offer(v) // Enqueue v
+    isVisited[v] = true // Mark it visited
+    while (!queue.isEmpty()) {
+        val u = queue.poll() // Dequeue to u
+        searchOrder.add(u) // u searched
+        adjacencyList[u].forEach { e -> // Note that e.u is u
+            if (!isVisited[e]) { // e.v is w in Listing 28.11
+                queue.offer(e) // Enqueue w
+                parent[e] = u // The parent of w is u
+                isVisited[e] = true // Mark w visited
+            }
+        }
+    }
+
+}
+
+private fun dijkstra(size: Int, adjacencyList: List<List<List<Int>>>, startingVertex: Int = 0) {
+    val cost = DoubleArray(size) { Double.POSITIVE_INFINITY }
+    cost[startingVertex] = 0.0 // Cost of source is 0
+    val parent = IntArray(size) // Parent of a vertex
+    parent[startingVertex] = -1 // startingVertex is the root
+    val processed = BooleanArray(size)
+    val t = PriorityQueue<Pair<Double, Int>>(Comparator.comparing { it.first })
+    val list = ArrayList<Int>()
+    t.add(Pair(0.0, startingVertex))
+    while (!t.isEmpty()) {
+        val a = t.poll().second
+        if (processed[a]) continue
+        processed[a] = true
+        list.add(a)
+        for (u in adjacencyList[a]) {
+            val b = u[1]
+            val w = u[2]
+            if (cost[a] + w < cost[b]) {
+                parent[b] = a
+                cost[b] = cost[a] + w
+                t.add(Pair(cost[b], b))
+            }
+        }
+    }
+}
+
+private fun floydWarshall(adjacencyMatrix: Array<DoubleArray>): Array<DoubleArray> {
+
+    val distance = adjacencyMatrix
+    val size = distance.size
+    for (k in 0 until size) {
+        for (i in 0 until size) {
+            for (j in 0 until size) {
+                distance[i][j] = java.lang.Double.min(distance[i][j], distance[i][k] + distance[k][j])
+            }
+        }
+    }
+
+    return distance
+}
+
+private fun bellmanFord(graph: Array<IntArray>, V: Int, E: Int, src: Int) {
+    // Initialize distance of all vertices as infinite.
+    val dis = IntArray(V) { Int.MAX_VALUE }
+
+    // initialize distance of source as 0
+    dis[src] = 0
+
+    // Relax all edges |V| - 1 times. A simple
+    // shortest path from src to any other
+    // vertex can have at-most |V| - 1 edges
+    for (i in 0 until V - 1) {
+        for (j in 0 until E) {
+            if (dis[graph[j][0]] != Int.MAX_VALUE && dis[graph[j][0]] + graph[j][2] <
+                dis[graph[j][1]]
+            ) dis[graph[j][1]] = dis[graph[j][0]] + graph[j][2]
+        }
+    }
+
+    // check for negative-weight cycles.
+    // The above step guarantees shortest
+    // distances if graph doesn't contain
+    // negative weight cycle. If we get a
+    // shorter path, then there is a cycle.
+    for (i in 0 until E) {
+        val x = graph[i][0]
+        val y = graph[i][1]
+        val weight = graph[i][2]
+        if (dis[x] != Int.MAX_VALUE && dis[x] + weight < dis[y]) println("Graph contains negative weight cycle")
+    }
+
+}
+
+//Primes
+private fun Int.isPrime(): Boolean {
+    if (this < 2) return false
+    var x = 2
+    while (x * x <= this) {
+        if (this % x == 0) return false
+        x++
+    }
+    return true
+}
+
+private fun Int.primeFactors(): LinkedList<Int> {
+    var n = this
+    val f = LinkedList<Int>()
+    var x = 2
+    while (x * x <= n) {
+        while (n % x == 0) {
+            f.addLast(x)
+            n /= x
+        }
+        x++
+    }
+    if (n > 1) f.addLast(n)
+    return f
+}
+
+//Permutations
+private fun permutationsIteration(n: Int, m: Int = n, f: (LinkedList<out Int>) -> Unit) {
+    val list = LinkedList<Int>()
+    var count = 0
+    val visited = BooleanArray(n)
+
+    fun getPerm(i: Int = 0) {
+        if (i == m) {
+            f(list)
+            count++
+            return
+        }
+        for (j in 0 until n) {
+            if (visited[j])
+                continue
+            list.add(j)
+            visited[j] = true
+            getPerm(i + 1)
+            visited[j] = false
+            list.removeLast()
+        }
+    }
+
+    getPerm()
+}
+
+//combinations
+private fun combinationsIteration(n: Int, m: Int, f: (LinkedList<out Int>) -> Unit) {
+    val list = LinkedList<Int>()
+    var count = 0
+
+    fun getCombination(i: Int = 0, lastVal: Int = 0) {
+        if (i == m) {
+            f(list)
+            count++
+            return
+        }
+        for (j in lastVal until n) {
+            list.add(j)
+            getCombination(i + 1, j + 1)
+            list.removeLast()
+        }
+    }
+    getCombination()
+}
+
+private fun getCombinationCountArray(n: Int, m: Int) {
+    require(n >= m)
+    val arr = Array(m + 1) { LongArray(n + 1) }
+    for (i in 0 until m + 1)
+        for (j in 0 until n + 1)
+            arr[i][j] = when {
+                j == 0 -> 1
+                i == 0 -> 0
+                else -> arr[i - 1][j - 1] + arr[i - 1][j]
+            }
+
+}
+
+//Search Techs
+
+private fun BSfindFirst(start: Int, end: Int, x: Int, f: (Int) -> Int): Int {
+    var s = start
+    var e = end
+    while (s < e) {
+        val mid = s + (e - s) / 2
+        if (f(mid) < x) s = mid + 1
+        else if (f(mid) > x) e = mid - 1
+        else e = mid
+    }
+    return s
+}
+
+private fun BSfindLast(start: Int, end: Int, x: Int, f: (Int) -> Int): Int {
+    var s = start
+    var e = end
+    while (s < e) {
+        val mid = s + (e - s) / 2
+        if (f(mid) < x) s = mid + 1
+        else if (f(mid) > x) e = mid - 1
+        else s = mid
+    }
+    return s
+}
+
+private fun binary(start: Double, end: Double, can: (Double) -> Boolean): Double {
+    var s = start
+    var e = end
+    repeat(100) {
+        val mid = (s + e) / 2
+        if (can(mid)) e = mid else s = mid
+    }
+
+    if (can(e))
+        return s
+
+    return -1.0    // failed
+}
+
+private fun BSfindFirstTrue(start: Int, end: Int, f: (Int) -> Boolean): Int {
+    var s = start
+    var e = end
+    while (s < e) {
+        val mid = s + (e - s) / 2
+        if (f(mid)) e = mid
+        else s = mid + 1
+    }
+    return s
+}
+
+private fun BSfindLastFalse(start: Int, end: Int, f: (Int) -> Boolean): Int {
+    var s = start
+    var e = end
+    while (s < e) {
+        val mid = s + (e - s) / 2
+        if (f(mid)) e = mid - 1
+        else s = mid
+    }
+    return s
+}
+
+//Complex
+/*
+private fun isEven(i: Int) = i % 2 == 0
+private val img = Complex(0.0, 1.0)
+private fun abs(c: Complex): Double = c.abs()
+private fun exp(c: Complex): Complex {
+    val e = exp(c.real)
+    return Complex(e * cos(c.img), e * sin(c.img))
+}
+private fun sinh(c: Complex) = (exp(c) - exp(-c)) / 2
+private fun cosh(c: Complex) = (exp(c) + exp(-c)) / 2
+private fun tanh(c: Complex) = sinh(c) / cosh(c)
+private fun coth(c: Complex) = cosh(c) / sinh(c)
+private fun cos(c: Complex) = (exp(img * c) + exp(-img * c)) / 2.0
+private fun sin(c: Complex) = img * (exp(-img * c) - exp(img * c)) / 2.0
+private fun tan(c: Complex) = sin(c) / cos(c)
+private fun cot(c: Complex) = cos(c) / sin(c)
+private fun sec(c: Complex) = Complex.ONE / cos(c)
+private fun ln(c: Complex) = Complex(ln(c.abs()), c.phase())
+private fun roots(n: Int) =
+    (1 ..n).map { exp(img*2*PI*it/n) }
+private operator fun Number.plus(c: Complex) = Complex(this.toDouble() + c.real, c.img)
+private operator fun Number.minus(c: Complex) = Complex(this.toDouble() - c.real, -c.img)
+private operator fun Number.times(c: Complex) = Complex(this.toDouble() * c.real, this.toDouble() * c.img)
+private operator fun Number.div(c: Complex) = Complex.ONE / c
+private class Complex(val real: Double, val img: Double) {
+    constructor(real: Number, img: Number) : this(real.toDouble(), img.toDouble())
+    override fun equals(other: Any?): Boolean {
+        return (other is Complex && real == other.real && img == other.img)
+    }
+    override fun hashCode(): Int {
+        return real.hashCode() * 31 + img.hashCode()
+    }
+    operator fun unaryMinus() = Complex(-real, -img)
+    operator fun plus(c: Complex) = Complex(real + c.real, img + c.img)
+    operator fun plus(n: Number) = Complex(real + n.toDouble(), img)
+    operator fun minus(c: Complex) = Complex(real - c.real, img - c.img)
+    operator fun minus(n: Number) = Complex(real - n.toDouble(), img)
+    operator fun times(c: Complex) = Complex(real * c.real - img * c.img, real * c.img + img * c.real)
+    operator fun times(n: Number) = Complex(n.toDouble() * real, n.toDouble() * img)
+    operator fun div(n: Number) = Complex(real / n.toDouble(), img / n.toDouble())
+    operator fun div(c: Complex): Complex {
+        val den = c.normSquared()
+        if (isPracticallyZero(den)) {
+            return this / 0 //TO make this consistent with division by zero number
+        }
+        val num = this * c.conjugate()
+        return num / den
+    }
+    operator fun component1() = real
+    operator fun component2() = img
+    /**
+     * Complex conjugate = x-y*i
+     */
+    fun conjugate() = Complex(real, -img)
+    fun normSquared() = real * real + img * img
+    fun abs(): Double = sqrt(this.normSquared())
+    fun phase(): Double = atan(img / real)
+    fun pow(a: Double) = exp(ln(this) * a)
+    fun pow(a: Number) = exp(ln(this) * a)
+    fun pow(a: Complex) = exp(ln(this) * a)
+    override fun toString(): String {
+        return when {
+            isPracticallyZero(img) -> "${ real}"
+            isPracticallyZero(real) -> "${ img}i"
+            img < 0 -> "${ real}-${ -img}i"
+            else -> "${ real}+${ img}i"
+        }
+    }
+    private fun isPracticallyZero(d: Double) = abs(d) < DEFAULT_TOLERANCE
+    companion object {
+        /**
+         * Complex 0 = 0 + 0i
+         */
+        val ZERO = Complex(0.0, 0.0)
+        /**
+         * Complex 1 = 1 + 0i
+         */
+        val ONE = Complex(1.0, 0.0)
+        const val DEFAULT_TOLERANCE = 1.0E-15
+        fun fromNumber(n: Number) = Complex(n.toDouble(), 0.0)
+        fun fromPolar(radius: Double, theta: Double) :Complex  =radius*exp(img*theta)
+    }
+    /**
+     * Tests if the norm of the complex number is smaller than the given tolerance
+     */
+    fun isZero(tolerance: Double) = this.abs() < tolerance
+    infix fun to(exponent: Int): Complex {
+        if (exponent == 0) {
+            return ONE
+        }
+        if (exponent == 1) {
+            return this
+        }
+        val half = to(exponent / 2)
+        return if (isEven(exponent)) {
+            half * half
+        } else {
+            half * half * this
+        }
+    }
+    infix fun to(exponent: Complex) = this.pow(exponent)
+    infix fun to(exponent: Number) = this.pow(exponent)
+}
+*/
+
+//Point2D
+/*
+val Complex.X
+    get() = this.real
+val Complex.Y
+    get() = this.img
+fun Complex.angle()=this.phase()
+fun vec(A:Complex,B:Complex)=B-A
+fun dotProduct(A:Complex,B:Complex)=(A.conjugate()*B).real
+fun crossProduct(A:Complex,B:Complex)=(A.conjugate()*B).img
+fun Complex.length()=this.abs()
+fun Complex.normalize()=this/this.abs()
+fun rotateAroundO(p:Complex,ang:Double)=p* exp(ang*img)
+fun rotateAroundA(p:Complex,ang:Double,about:Complex)=rotateAroundO(vec(about,p),ang)+about
+fun reflectO(v:Complex,m:Complex)=(((v)/(m)).conjugate()*(m))
+fun reflect(p:Complex, p0:Complex, p1:Complex):Complex {
+    val z = p-p0
+    val w = p1-p0
+    return (z/w).conjugate()*w + p0 // Refelect point p1 around p0p1
+}
+fun fixAngle(A:Double): Double =if(A > 1) 1.0 else if (A < -1) -1.0 else A
+fun angleO( a:Complex, O:Complex, b:Complex):Complex {
+    val v1=(a - O)
+    val v2=(b - O)
+    return Complex.fromPolar(1.0, acos(fixAngle ( dotProduct(v1, v2) / v1.length() / v2.length() )) )
+}
+fun getAng(a:Complex, b:Complex, c:Complex):Double {
+    var ang =  vec(b, c).angle() -  vec(b, a).angle()
+    if(ang< 0)
+        ang+=2*PI
+    return ang
+}
+ */
+
+//lines
+
+/*
+
+fun isCollinear(a: Complex, b: Complex, c: Complex): Boolean {
+    return (crossProduct(b - a, c - a)).isZero()
+}
+
+fun isPointOnRay(p0: Complex, p1: Complex, p2: Complex): Boolean {
+    return if ((p2 - p0).length().isZero()) true else ((p1 - p0).normalize()== (p2 - p0).normalize())
+}
+
+fun isPointOnSegment(a: Complex, b: Complex, c: Complex): Boolean {
+    return isPointOnRay(a, b, c) && isPointOnRay(b, a, c)
+}
+fun distToLine(p0: Complex, p1: Complex, p2: Complex): Double {
+    return abs(crossProduct(p1 - p0, p2 - p0) / (p0 - p1).length()) // area = 0.5*b*h
+}
+
+fun distToSegment(p0: Complex, p1: Complex, p2: Complex): Double {
+    var d1: Double
+    var d2: Double
+    val v1: Complex = p1 - p0
+    val v2: Complex = p2 - p0
+    if (dotProduct(v1, v2).also { d1 = it } <= 0) return (p2 - p0).length()
+    if (dotProduct(v1, v1).also { d2 = it } <= d1) return (p2 - p1).length()
+    val t = d1 / d2
+    return (p2 - (p0 + v1 * t)).length()
+}
+ */
