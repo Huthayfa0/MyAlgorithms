aa1d39f095a933e2e5e7eae12928220ff5040f21
Huthayfa0 <huthifa0@gmail.com>
2021-07-25 16:02:29 +0300

    Prim

--- src/template.kt
+++ src/template.kt
@@ -198,6 +198,38 @@ private fun bellmanFord(graph: Array<IntArray>, V: Int, E: Int, src: Int) {
 
 }
 
+private fun prim(size: Int, adjacencyList: List<List<List<Int>>>, startingVertex: Int = 0) {
+    val cost = DoubleArray(size) { Double.POSITIVE_INFINITY }
+    cost[startingVertex] = 0.0 // Cost of source is 0
+    val parent = IntArray(size) // Parent of a vertex
+    parent[startingVertex] = -1 // startingVertex is the root
+    val t = mutableSetOf<Int>()
+
+    // Expand T
+    while (t.size < size) {
+        // Find smallest cost u in V - T
+        var u = -1 // Vertex to be determined
+        var currentMinCost = Double.POSITIVE_INFINITY
+        for (i in 0 until size) {
+            if (!t.contains(i) && cost[i] < currentMinCost) {
+                currentMinCost = cost[i]
+                u = i
+            }
+        }
+        if (u == -1) break else t.add(u)
+
+        // Adjust cost[v] for v that is adjacent to u and v in V - T
+        for (e in adjacencyList[u]) {
+            val sum = e[2] + cost[u]
+            if (!t.contains(e[1]) && cost[e[1]] > sum) {
+                cost[e[1]] = sum
+                parent[e[1]] = u
+            }
+        }
+    }
+}
+
+
 //Primes
 private fun Int.isPrime(): Boolean {
     if (this < 2) return false
