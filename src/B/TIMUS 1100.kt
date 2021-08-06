89f1004cdbdd790e30916338668181d6430225e8
Huthayfa0 <huthifa0@gmail.com>
2021-08-06 20:45:57 +0300

    TIMUS 1100

---
+++ src/B/TIMUS 1100.kt
@@ -0,0 +1,91 @@
+import java.util.*
+import kotlin.Comparator
+import kotlin.math.*
+
+fun main() {
+    val n=readlnInt()
+
+    val arr= Array(n){readIntArray()}.sortedBy { -it[1] }
+   // arr=mergeSort(arr, Comparator.comparing<List<Int>, Int> { it[1] }.reversed())
+
+    println(arr.joinToString(separator = "\n") { it.joinToString(separator = " ") })
+}
+
+//Scanner code
+private fun readln() = readLine()!!
+private fun readlnInt() = readln().toInt()
+private fun readlnLong() = readln().toLong()
+private fun readlnDouble() = readln().toDouble()
+private fun readlnBigInt(radix: Int = 10) = readln().toBigInteger(radix)
+private fun readlnBigDecimal() = readln().toBigDecimal()
+private fun lineSequence(limit: Int = Int.MAX_VALUE) = generateSequence { readLine() }.constrainOnce().take(limit)
+private fun readlnStrings() = readln().split(' ')
+private fun readlnInts() = readlnStrings().map { it.toInt() }
+private fun readlnLongs() = readlnStrings().map { it.toLong() }
+private fun readlnDoubles() = readlnStrings().map { it.toDouble() }
+private fun readIntArray() = readlnStrings().run { IntArray(size) { get(it).toInt() } }
+private fun readLongArray() = readlnStrings().run { LongArray(size) { get(it).toLong() } }
+private fun readDoubleArray() = readlnStrings().run { DoubleArray(size) { get(it).toDouble() } }
+private fun readlnIntArray(n: Int) = IntArray(n) { readlnInt() }
+private fun readlnLongArray(n: Int) = LongArray(n) { readlnLong() }
+private fun readlnDoubleArray(n: Int) = DoubleArray(n) { readlnDouble() }
+private fun readLongArray2d(rows: Int, cols: Int) = Array(rows) { readLongArray().also { require(it.size == cols) } }
+private fun readIntArray2d(rows: Int, cols: Int) = Array(rows) { readIntArray().also { require(it.size == cols) } }
+private fun readDoubleArray2d(rows: Int, cols: Int) =
+    Array(rows) { readDoubleArray().also { require(it.size == cols) } }
+
+private fun isWhiteSpace(c: Char) = c in " \r\n\t"
+
+// JVM-only targeting code follows next
+// readString() via sequence is still slightly faster than Scanner
+private fun readString() = generateSequence { System.`in`.read().toChar() }
+    .dropWhile { isWhiteSpace(it) }.takeWhile { !isWhiteSpace(it) }.joinToString("")
+
+private fun readInt() = readString().toInt()
+private fun readLong() = readString().toLong()
+private fun readDouble() = readString().toDouble()
+private fun readBigInt(radix: Int = 10) = readString().toBigInteger(radix)
+private fun readBigDecimal() = readString().toBigDecimal()
+private fun readInts(n: Int) = generateSequence { readInt() }.take(n)
+private fun readLongs(n: Int) = generateSequence { readLong() }.take(n)
+private fun readDoubles(n: Int) = generateSequence { readDouble() }.take(n)
+
+
+//sort
+private fun <E>mergeSort(list: List<E>,compartor:Comparator<E>): List<E> {
+    fun merge(left: List<E>, right: List<E>): List<E> {
+        var indexLeft = 0
+        var indexRight = 0
+        val newList: MutableList<E> = mutableListOf()
+
+        while (indexLeft < left.count() && indexRight < right.count()) {
+            if (compartor.compare(left[indexLeft], right[indexRight])<=0 ) {
+                newList.add(left[indexLeft])
+                indexLeft++
+            } else {
+                newList.add(right[indexRight])
+                indexRight++
+            }
+        }
+
+        while (indexLeft < left.size) {
+            newList.add(left[indexLeft])
+            indexLeft++
+        }
+
+        while (indexRight < right.size) {
+            newList.add(right[indexRight])
+            indexRight++
+        }
+        return newList
+    }
+    if (list.size <= 1) {
+        return list
+    }
+
+    val middle = list.size / 2
+    val left = list.subList(0, middle)
+    val right = list.subList(middle, list.size)
+
+    return merge(mergeSort(left,compartor), mergeSort(right,compartor))
+}
