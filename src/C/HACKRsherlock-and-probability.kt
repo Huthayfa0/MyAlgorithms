890204f1fefc9dc950616e26560af6e7db81e555
Huthayfa0 <huthifa0@gmail.com>
2021-09-18 19:42:09 +0300

    HackerSherlockAndProb

---
+++ src/C/HACKRsherlock-and-probability.kt
@@ -0,0 +1,65 @@
+import java.util.*
+import kotlin.math.*
+
+//RotateArray MergeSort FirstSearch ShoretestPath
+//MinimumSpanningTree Primes Permutations
+//Combinations BinarySearchTechs ComplexAndGeometry 
+fun main() {
+val t=readlnInt()
+    repeat(t){
+        val (n,k)=readlnInts()
+        val arr=readln().map { it.digitToInt() }
+        var c=arr[0]
+        for (i in 1..min(k,n-1))
+            c += arr[i]
+        var ans=if (arr[0]==1)c.toLong() else 0L
+
+        for (i in 1 until n){
+            if (i-k>0) c-=arr[i-k-1]
+            if (i+k<n) c+=arr[i+k]
+            if (arr[i]==1) ans+= c.toLong()
+        }
+        fun gcd(a:Long,b:Long):Long=if (b==0L)a else gcd(b,a%b)
+        val g=gcd(n*n.toLong(),ans)
+        println("${ans/g}/${n.toLong()*n/g}")
+    }
+}
+
+//Scanner code, if wanted to read from file just change the first
+private fun readln() = readLine()!!.trim()
+private fun readlnInt() = readln().toInt()
+private fun readlnLong() = readln().toLong()
+private fun readlnDouble() = readln().toDouble()
+private fun readlnBigInt(radix: Int = 10) = readln().toBigInteger(radix)
+private fun readlnBigDecimal() = readln().toBigDecimal()
+private fun lineSequence(limit: Int = Int.MAX_VALUE) = generateSequence { readLine() }.constrainOnce().take(limit)
+private fun readlnStrings() = readln().split(' ')
+private fun readlnInts() = readlnStrings().map { it.toInt() }
+private fun readlnLongs() = readlnStrings().map { it.toLong() }
+private fun readlnDoubles() = readlnStrings().map { it.toDouble() }
+private fun readIntArray() = readlnStrings().run { IntArray(size) { get(it).toInt() } }
+private fun readLongArray() = readlnStrings().run { LongArray(size) { get(it).toLong() } }
+private fun readDoubleArray() = readlnStrings().run { DoubleArray(size) { get(it).toDouble() } }
+private fun readlnIntArray(n: Int) = IntArray(n) { readlnInt() }
+private fun readlnLongArray(n: Int) = LongArray(n) { readlnLong() }
+private fun readlnDoubleArray(n: Int) = DoubleArray(n) { readlnDouble() }
+private fun readLongArray2d(rows: Int, cols: Int) = Array(rows) { readLongArray().also { require(it.size == cols) } }
+private fun readIntArray2d(rows: Int, cols: Int) = Array(rows) { readIntArray().also { require(it.size == cols) } }
+private fun readDoubleArray2d(rows: Int, cols: Int) =
+    Array(rows) { readDoubleArray().also { require(it.size == cols) } }
+
+private fun isWhiteSpace(c: Char) = c in " \r\n\t"
+
+// JVM-only targeting code follows next
+// readString() via sequence is still slightly faster than Scanner
+private fun readString() = generateSequence { System.`in`.read().toChar() }
+    .dropWhile { isWhiteSpace(it) }.takeWhile { !isWhiteSpace(it) }.joinToString("")
+
+private fun readInt() = readString().toInt()
+private fun readLong() = readString().toLong()
+private fun readDouble() = readString().toDouble()
+private fun readBigInt(radix: Int = 10) = readString().toBigInteger(radix)
+private fun readBigDecimal() = readString().toBigDecimal()
+private fun readInts(n: Int) = generateSequence { readInt() }.take(n)
+private fun readLongs(n: Int) = generateSequence { readLong() }.take(n)
+private fun readDoubles(n: Int) = generateSequence { readDouble() }.take(n)
