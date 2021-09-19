e39c9c41b58f38aff1bc1d5a4088dd37007d7ec2
Huthayfa0 <huthifa0@gmail.com>
2021-09-19 23:19:45 +0300

    CF577-D2-C

---
+++ src/C/CF577-D2-C.kt
@@ -0,0 +1,97 @@
+import java.util.*
+import kotlin.math.*
+
+//RotateArray MergeSort FirstSearch ShoretestPath
+//MinimumSpanningTree Primes Permutations
+//Combinations BinarySearchTechs ComplexAndGeometry 
+fun main() {
+    val n=readlnInt()
+    val ans= mutableListOf<Int>()
+    var i=2
+    if (i.isPrime()){
+        var k=i
+        while (k<=n){
+            ans.add(k)
+            k*=i
+        }
+    }
+    i++
+    while (i<=n){
+        if (i.isPrime()){
+            var k=i
+            while (k<=n){
+                ans.add(k)
+                k*=i
+            }
+        }
+        i+=2
+    }
+    println(ans.size)
+    println(ans.joinToString(separator = " "))
+}
+
+
+private fun Int.isPrime(): Boolean {
+    if (this < 2) return false
+    var x = 2
+    while (x * x <= this) {
+        if (this % x == 0) return false
+        x++
+    }
+    return true
+}
+
+private fun Int.primeFactors(): LinkedList<Int> {
+    var n = this
+    val f = LinkedList<Int>()
+    var x = 2
+    while (x * x <= n) {
+        while (n % x == 0) {
+            f.addLast(x)
+            n /= x
+        }
+        x++
+    }
+    if (n > 1) f.addLast(n)
+    return f
+}
+
+
+//Scanner code, if wanted to read from file just change the first
+private fun readln() = readLine()!!
+private fun readlnInt() = readln().toInt()
+private fun readlnLong() = readln().toLong()
+private fun readlnDouble() = readln().toDouble()
+private fun readlnBigInt(radix: Int = 10) = readln().toBigInteger(radix)
+private fun readlnBigDecimal() = readln().toBigDecimal()
+private fun lineSequence(limit: Int = Int.MAX_VALUE) = generateSequence { readLine() }.constrainOnce().take(limit)
+private fun readlnStrings() = readln().split(' ')
+private fun readlnInts() = readlnStrings().map { it.toInt() }
+private fun readlnLongs() = readlnStrings().map { it.toLong() }
+private fun readlnDoubles() = readlnStrings().map { it.toDouble() }
+private fun readIntArray() = readlnStrings().run { IntArray(size) { get(it).toInt() } }
+private fun readLongArray() = readlnStrings().run { LongArray(size) { get(it).toLong() } }
+private fun readDoubleArray() = readlnStrings().run { DoubleArray(size) { get(it).toDouble() } }
+private fun readlnIntArray(n: Int) = IntArray(n) { readlnInt() }
+private fun readlnLongArray(n: Int) = LongArray(n) { readlnLong() }
+private fun readlnDoubleArray(n: Int) = DoubleArray(n) { readlnDouble() }
+private fun readLongArray2d(rows: Int, cols: Int) = Array(rows) { readLongArray().also { require(it.size == cols) } }
+private fun readIntArray2d(rows: Int, cols: Int) = Array(rows) { readIntArray().also { require(it.size == cols) } }
+private fun readDoubleArray2d(rows: Int, cols: Int) =
+    Array(rows) { readDoubleArray().also { require(it.size == cols) } }
+
+private fun isWhiteSpace(c: Char) = c in " \r\n\t"
+
+// JVM-only targeting code follows next
+// readString() via sequence is still slightly faster than Scanner
+private fun readString() = generateSequence { System.`in`.read().toChar() }
+    .dropWhile { isWhiteSpace(it) }.takeWhile { !isWhiteSpace(it) }.joinToString("")
+
+private fun readInt() = readString().toInt()
+private fun readLong() = readString().toLong()
+private fun readDouble() = readString().toDouble()
+private fun readBigInt(radix: Int = 10) = readString().toBigInteger(radix)
+private fun readBigDecimal() = readString().toBigDecimal()
+private fun readInts(n: Int) = generateSequence { readInt() }.take(n)
+private fun readLongs(n: Int) = generateSequence { readLong() }.take(n)
+private fun readDoubles(n: Int) = generateSequence { readDouble() }.take(n)
