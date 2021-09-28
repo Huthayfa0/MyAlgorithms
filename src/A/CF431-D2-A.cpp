e52a161717852174b2a9c71e5369fea9395f185f
Huthayfa0 <huthifa0@gmail.com>
2021-09-28 23:14:53 +0300

    CF431-D2-A

---
+++ src/A/CF431-D2-A.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define mp make_pair
+#define pb push_back
+#define lp(i,s,f) for(ll i = s; i < ll(f); i++)
+#define inF freopen("in", "r", stdin);
+#define outF freopen("out", "w", stdout);
+#define endl '\n'
+#define MOD 1000000007
+#define mm(arr) memset(arr, 0, sizeof(arr))
+#define sz(v) (int) v.size()
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+
+//DepthFirstSearch BreadthFirstSearch DijkstraShortestPath
+//FloydWarshallShortestPath BellmanFordShortestPath PrimMST
+//KruskalMST PrimeCheck Factorization
+
+int main() {
+    FAST
+    ll x1,x2,x3,x4;
+    cin>>x1>>x2>>x3>>x4;
+    string s;
+    cin>>s;
+    ll ans=0;
+    for (auto c:s) {
+        switch (c){
+            case '1':
+                ans+=x1;
+                break;
+                case '2':
+                    ans+=x2;
+                    break;
+                    case '3':
+                        ans+=x3;
+                        break;
+                        case '4':
+                            ans+=x4;
+                            break;
+        }
+
+    }
+    cout<<ans<<endl;
+    return 0;
+}
+
