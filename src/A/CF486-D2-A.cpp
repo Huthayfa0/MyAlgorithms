38f66485d42a97e3099832e03cddb0d143f40132
Huthayfa0 <huthifa0@gmail.com>
2021-10-03 22:36:58 +0300

    CF486-D2-A

---
+++ src/A/CF486-D2-A.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define mp make_pair
+#define pb push_back
+#define lp(i,s,f) for(ll i = s; i < ll(f); i++)
+#define inF freopen("in", "r", stdin);
+#define outF freopen("out", "w", stdout);
+#define endl '\n'
+#define MOD 1000000007
+#define mm(arr) memset(arr, 0, sizeof(arr))
+#define sz(v) (int) v.size()
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+
+//DepthFirstSearch BreadthFirstSearch DijkstraShortestPath
+//FloydWarshallShortestPath BellmanFordShortestPath PrimMST
+//KruskalMST PrimeCheck Factorization
+
+int main() {
+    FAST
+    ll n;
+    cin>>n;
+    cout<<((n%2==1)?"-":"")<<(n+1)/2<<endl;
+    return 0;
+}
+
+
