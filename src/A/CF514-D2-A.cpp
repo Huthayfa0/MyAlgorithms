357dd405dfc3643eb5a5c86f53d594dce08debbc
Huthayfa0 <huthifa0@gmail.com>
2021-10-04 23:40:42 +0300

    CF514-D2-A

---
+++ src/A/CF514-D2-A.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define mp make_pair
+#define pb push_back
+#define lp(i,s,f) for(ll i = s; i < ll(f); i++)
+#define inF freopen("in", "r", stdin);
+#define outF freopen("out", "w", stdout);
+#define endl '\n'
+#define MOD 1000000007
+#define mm(arr) memset(arr, 0, sizeof(arr))
+#define sz(v) (int) v.size()
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+
+//DepthFirstSearch BreadthFirstSearch DijkstraShortestPath
+//FloydWarshallShortestPath BellmanFordShortestPath PrimMST
+//KruskalMST PrimeCheck Factorization
+
+
+bool isPrime(ll num){
+    if (num < 2) return false;
+    ll x = 2;
+    while (x * x <= num) {
+        if (num % x == 0) return false;
+        x++;
+    }
+    return true;
+}
+
+int main() {
+    FAST
+    string x;
+    cin>>x;
+    ll i=0;
+    ll n=x.length();
+    while (i<n){
+        if(x[i]=='9') {
+            i++;
+            cout<<'9';
+        }
+        else
+            break;
+    }
+    while (i<n){
+        if(x[i]>'4'){
+            cout<<(char)(9-x[i]+2*'0');
+        }else
+            cout<<x[i];
+        i++;
+    }
+    cout<<endl;
+    return 0;
+}
+
+
