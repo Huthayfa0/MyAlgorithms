9212c3d316839212a53054aa66694817f82d25af
Huthayfa0 <huthifa0@gmail.com>
2021-11-01 21:56:46 +0200

    CF230-D2-B

---
+++ src/C/CF230-D2-B.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<ll> VL;
+typedef vector<VL> ML;
+#define FAST ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define mp make_pair
+#define pb push_back
+#define lp(i,s,f) for(ll i = s; i < ll(f); i++)
+#define inF freopen("in", "r", stdin);
+#define outF freopen("out", "w", stdout);
+#define endl '\n'
+#define MOD 1000000007
+#define mm(arr) memset(arr, 0, sizeof(arr))
+#define sz(v) (int) v.size()
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+
+//DepthFirstSearch BreadthFirstSearch DijkstraShortestPath
+//FloydWarshallShortestPath BellmanFordShortestPath PrimMST
+//KruskalMST PrimeCheck Factorization NumberPower Matrix
+
+bool isPrime(ll num){
+    if (num < 2) return false;
+    ll x = 2;
+    while (x * x <= num) {
+        if (num % x == 0) return false;
+        x++;
+    }
+    return true;
+}
+
+
+bool perfectSquare(ll n) {
+    ll x= sqrt(n);
+    return x*x==n&& isPrime(x);
+}
+
+void solve()
+{
+    ll n;
+    cin>>n;
+    cout<<(perfectSquare(n)?"YES":"NO")<<endl;
+}
+
+
+int main(){
+    FAST
+    ll t;
+    cin>>t;
+    while (t-->0){
+        solve();
+    }
+
+    return 0;
+}
