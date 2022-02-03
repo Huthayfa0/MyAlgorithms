dcf3290fc71f0030c8d4a3320c40fd068fa6cef3
Huthayfa0 <huthifa0@gmail.com>
2022-02-03 17:23:34 +0200

    UVA113

---
+++ src/C/UVA113.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef vector<ll> VL;
+typedef vector<VL> ML;
+#define FAST ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define mp make_pair
+#define pb push_back
+#define lp(i,s,f) for(ll i = s; (i) < ll(f); (i)++)
+#define re(i,a) lp(i,0,sz(a)) cin >> (a)[i]
+#define out(i,a) lp(i,0,sz(a)) cout << (a)[i] << ((i) == sz(a) - 1 ? '\n' : ' ')
+#define inF freopen("in", "r", stdin);
+#define outF freopen("out", "w", stdout);
+#define endl '\n'
+#define MOD 1000000007
+#define N 5000
+#define inf 0x3f3f3f3f3f3f3f3fLL
+#define mm(arr) memset(arr, 0, sizeof(arr))
+#define sz(v) (int) (v).size()
+#define all(v) (v).begin(), (v).end()
+#define F first
+#define S second
+#define decimalpoint cout << std::fixed << setprecision(0)
+void chmin(ll&a,ll b){if(a>b)a=b;}
+void chmax(ll&a,ll b){if(a<b)a=b;}
+//DepthFirstSearch BreadthFirstSearch DijkstraShortestPath
+//FloydWarshallShortestPath PrimMST MatrixExponentiation
+//KruskalMST PrimeCheck Factorization NumberPower
+//DivisionInverse Combinations
+
+void solve(){
+    ld a,b;
+    while (cin>>a>>b){
+        if(a==0)break;
+        decimalpoint<<pow(b,1/a);
+        cout<<endl;
+    }
+
+}
+
+int main(){
+
+    FAST
+    ll t;
+    t=1;
+    while(t--){
+       solve();
+    }
+    return 0;
+}
