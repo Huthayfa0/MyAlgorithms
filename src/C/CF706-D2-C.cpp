2d881d21a5d81364408ecd777be86c9f2e2be6ce
Huthayfa0 <huthifa0@gmail.com>
2022-07-14 23:19:13 +0300

    CF706-D2-C

--- src/C/CF706-D2-C.cpp
+++ src/C/CF706-D2-C.cpp
@@ -1,99 +1,78 @@
 #include<bits/stdc++.h>
+#include <iostream>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
 typedef long long ll;
+typedef long double ld;
 typedef vector<ll> VL;
 typedef vector<VL> ML;
-
+const double PI = acos(-1.0);
+const ld EPS = 1e-18;
 
 #define FAST ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define mp make_pair
 #define pb push_back
-#define lp(i,s,f) for(ll i = s; i < ll(f); i++)
+#define lp(i,s,f) for(ll i = s; (i) < ll(f); (i)++)
+#define re(i,a) lp(i,0,sz(a)) cin >> (a)[i]
+#define out(i,a) lp(i,0,sz(a)) cout << (a)[i] << ((i) == sz(a) - 1 ? '\n' : ' ')
 #define inF freopen("in", "r", stdin);
 #define outF freopen("out", "w", stdout);
 #define endl '\n'
-#define MOD 1000000000
+#define MOD 1000000007
+#define inf 0x3f3f3f3f3f3f3f3fLL
 #define mm(arr) memset(arr, 0, sizeof(arr))
-#define sz(v) (int) v.size()
-#define all(v) v.begin(), v.end()
+#define sz(v) (int) (v).size()
+#define all(v) (v).begin(), (v).end()
 #define F first
 #define S second
+#define decimalpoint cout << std::fixed << setprecision(2)
+typedef complex<ld> point;
 
+using namespace __gnu_pbds;
+#define ordered_set tree<pair<ll,ll>, null_type,less<>, rb_tree_tag,tree_order_statistics_node_update>
+void chmin(ll&a,ll b){if(a>b)a=b;}
+void chmax(ll&a,ll b){if(a<b)a=b;}
 //DepthFirstSearch BreadthFirstSearch DijkstraShortestPath
-//FloydWarshallShortestPath BellmanFordShortestPath PrimMST
-//KruskalMST PrimeCheck Factorization NumberPower Matrix
-bool cmp(string x,string y,ll i){
-    if(i==0){
-        return x<=y;
-    }
-    int xx=x.length()-1,yy=y.length()-1;
-    if(i==3){
-    lp(j,0, min(xx,yy)) {
-        if(x[xx-j]<y[yy-j])return true;
-        if(x[xx-j]>y[yy-j])return false;
-    }
-    }
-    if(i==2){
-        lp(j,0, min(xx,yy)) {
-            if(x[xx-j]<y[j])return true;
-            if(x[xx-j]>y[j])return false;
-        }
-    }
-    if(i==1){
-        lp(j,0, min(xx,yy)) {
-            if(x[j]<y[yy-j])return true;
-            if(x[j]>y[yy-j])return false;
-        }
-    }
-    return xx<=yy;
-}
-ML dp;
-ll n;
-VL arr;
-vector<string> str;
-ll cal(ll i,ll pre){
-    if(i==n)return 0;
-    ll&x=dp[i][pre];
-    if(x!=-1)return x;
-    if(pre==1){
-        bool xx= cmp(str[i-1],str[i],3);
-        bool yy= cmp(str[i-1],str[i],2);
-        if(xx&&yy){
-            return x=min(cal(i+1,0),arr[i]+cal(i+1,1));
-        }else if(xx){
-            return x=arr[i]+cal(i+1,1);
-        }else if(yy)return x=cal(i+1,0);
-        else return x=1e18+5;
-    }else{
-        bool xx= cmp(str[i-1],str[i],1);
-        bool yy= cmp(str[i-1],str[i],0);
-        if(xx&&yy){
-            return x=min(cal(i+1,0),arr[i]+cal(i+1,1));
-        }else if(xx){
-            return x=arr[i]+cal(i+1,1);
-        }else if(yy)return x=cal(i+1,0);
-        else return x=1e18+5;
-    }
-}
+//FloydWarshallShortestPath PrimMST MatrixExponentiation
+//KruskalMST PrimeCheck Factorization NumberPower
+//DivisionInverse Combinations Geometry
+//LinesGeometry CirclesGeometry BigInteger
+
 void solve(){
-    cin>>n;
-    dp=ML(n,VL(2,-1));
-    arr=VL(n),str=vector<string>(n);
-    lp(i,0,n)cin>>arr[i];
-    lp(i,0,n)cin>>str[i];
-    ll ans= min(cal(1,0),arr[0]+ cal(1,1));
-    if(ans==1e18+5)
-        cout<<-1<<endl;
-    else
-    cout<<ans<<endl;
+   ll n;cin>>n;VL arr(n);re(i,arr);vector<string> strings(n),reversed(n);re(i,strings);
+   reversed=strings;
+   lp(i,0,n)std::reverse(reversed[i].begin(), reversed[i].end());
+   ll ans[100005][2];
+   lp(i,0,100005)lp(j,0,2)ans[i][j]=inf;
+   ans[0][0]=0;
+   ans[0][1]=arr[0];
+   lp(i,1,n){
+       if(strings[i-1]<=strings[i]){
+           ans[i][0]=ans[i-1][0];
+       }
+       if(reversed[i-1]<=strings[i]){
+           chmin(ans[i][0],ans[i-1][1]);
+       }
+       if(strings[i-1]<=reversed[i]){
+           ans[i][1]=ans[i-1][0]+arr[i];
+       }
+       if(reversed[i-1]<=reversed[i]){
+           chmin(ans[i][1],ans[i-1][1]+arr[i]);
+       }
+   }
+   cout<<(min(ans[n-1][0],ans[n-1][1])==inf?-1:min(ans[n-1][0],ans[n-1][1]))<<endl;
 }
 
 int main(){
     FAST
-    ll t;
-    t=1;
+    ll t=1;
+    //cin>>t;
+    //ll nn=t;
+
     while(t--){
-        solve();
+      //cout<<"Case #"<<nn-t<<": ";//<<endl;
+      solve();
     }
 
     return 0;
