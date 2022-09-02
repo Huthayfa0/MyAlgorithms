c05587df1c6b256f0af8e9f93644126aed444e5e
huthifa0 <huthifa0@gmail.com>
2022-09-02 20:39:31 +0300

    Gray_Code.cpp

---
+++ src/C/Gray_Code.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;std::cin>>n;
+    vector<string> arr;
+    arr.push_back("0");
+    arr.push_back("1");
+    int i, j;
+    for (i = 2; i < (1<<n); i = i<<1)
+    {
+        for (j = i-1 ; j >= 0 ; j--)
+            arr.push_back(arr[j]);
+        for (j = 0 ; j < i ; j++)
+            arr[j] = "0" + arr[j];
+        for (j = i ; j < 2*i ; j++)
+            arr[j] = "1" + arr[j];
+    }
+ 
+    for (i = 0 ; i < arr.size() ; i++ )
+        cout << arr[i] << "\n";
+}
