0d866fc1f80f1caaa826369dd9646023796a45d5
huthifa0 <huthifa0@gmail.com>
2022-09-15 20:09:47 +0300

    Coin_Combinations_II.cpp

---
+++ src/C/Coin_Combinations_II.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define MOD 1000000007
+int main(){
+    ll n,x;cin>>n>>x;ll dp[x+1]={0};
+    ll arr[n];for(ll i=0;i<n;i++)cin>>arr[i];
+    dp[0]=1;
+    for(ll j=0;j<n;j++){
+    for(ll i=1;i<=x;i++){
+        
+            if(i-arr[j]>=0){
+                dp[i]+=dp[i-arr[j]];
+                dp[i]%=MOD;
+            }
+        }
+    }
+    cout<<dp[x];
+    return 0;
+}
